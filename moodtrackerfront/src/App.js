1ccdfc464dbfd18e59eced2d02fa67b208142ccf
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-17 23:12:44 +0100

    components

--- moodtrackerfront/src/App.js
+++ moodtrackerfront/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Details from './components/Details';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React or dont, its totally up to you
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div> {/* Wrap both Navbar and Routes in a parent div */}
+        <Navigation /> {/* Render the Navbar component */}
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/details" component={Details} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ moodtrackerfront/src/components/Details.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+function Details() {
+  return <h1>This is the Details page</h1>;
+}
+
+export default Details;
