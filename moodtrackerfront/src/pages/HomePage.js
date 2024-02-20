f7c2565d583a5f9a5ff71a0dc31e644dc947f838
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-20 14:35:48 +0100

    commit

--- moodtrackerfront/src/pages/HomePage.js
+++ moodtrackerfront/src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import MoodsList from '../components/MoodsList';
 
 
 function Home() {
   return (
     <div style={{ marginLeft: '15vw', paddingTop: '15vh' }}>
       <h2 style={{ color: 'black' }}>This is the Home page</h2>
-    
+      <MoodsList />
     </div>
   );
 }
