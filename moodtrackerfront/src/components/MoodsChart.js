b65ec74a70ccd0e2cc1a7425ecfdede8ab7019f4
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-21 14:48:14 +0100

    commit

--- moodtrackerfront/src/components/MoodsChart.js
+++ moodtrackerfront/src/components/MoodsChart.js
@@ -32,26 +32,73 @@ function MoodsChart() {
     }
 
     const labels = moodData.map(mood => mood.date); // Assuming you have a 'date' field in mood objects
-    const data = moodData.map(mood => mood.mood); // Assuming you have a 'mood' field in mood objects
+    const lowMoods = moodData.map(mood => mood.low); // Assuming you have a 'low' field in mood objects
+    const highMoods = moodData.map(mood => mood.high); // Assuming you have a 'high' field in mood objects
+    const hoursOfSleep = moodData.map(mood => mood.sleep); // Assuming you have a 'hoursOfSleep' field in mood objects
+    const minutesOfPhysicalActivity = moodData.map(mood => mood.move); // Assuming you have a 'minutesOfPhysicalActivity' field in mood objects
 
     // Chart.js setup
     const ctx = document.getElementById('moodsChart');
     chartRef.current = new Chart(ctx, {
-      type: 'line', // Change to 'bar' for a bar chart
+      type: 'line',
       data: {
         labels: labels,
         datasets: [{
-          label: 'Mood',
-          data: data,
+          label: 'Low Mood',
+          data: lowMoods,
+          borderColor: 'rgb(255, 99, 132)',
+          tension: 0.1,
+          yAxisID: 'mood'
+        }, {
+          label: 'High Mood',
+          data: highMoods,
+          borderColor: 'rgb(54, 162, 235)',
+          tension: 0.1,
+          yAxisID: 'mood'
+        }, {
+          label: 'Hours of Sleep',
+          data: hoursOfSleep,
           borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
+          tension: 0.1,
+          yAxisID: 'hoursOfSleep'
+        }, {
+          label: 'Minutes of Physical Activity',
+          data: minutesOfPhysicalActivity,
+          borderColor: 'rgb(255, 205, 86)',
+          tension: 0.1,
+          yAxisID: 'minutesOfPhysicalActivity'
         }]
       },
       options: {
         scales: {
-          y: {
+          x: {
+            title: {
+              display: true,
+              text: 'Days of the Month'
+            }
+          },
+          mood: {
+            position: 'left',
+            title: {
+              display: true,
+              text: 'Mood'
+            },
             min: -5,
             max: 5
+          },
+          hoursOfSleep: {
+            position: 'right',
+            title: {
+              display: true,
+              text: 'Hours of Sleep'
+            }
+          },
+          minutesOfPhysicalActivity: {
+            position: 'right',
+            title: {
+              display: true,
+              text: 'Minutes of Physical Activity'
+            }
           }
         }
       }
