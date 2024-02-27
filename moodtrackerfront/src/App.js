a6d636ecd7a17556a9e80da8efadb05ce9f42717
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-27 12:51:43 +0100

    commit

--- moodtrackerfront/src/App.js
+++ moodtrackerfront/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/HomePage';
 import About from './pages/AboutPage';
 import Notes from './pages/DetailsPage';
 import MoodForm from './components/MoodForm'; // Import the MoodForm component
+import Login from './pages/LoginPage'; // Import the Login component
+import Register from './pages/RegisterPage'; // Import the Register component
 
 function App() {
   return (
@@ -16,8 +18,10 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/details" element={<Notes />} />
-            {/* Add a new route for the mood tracking page */}
             <Route path="/track-mood" element={<MoodForm />} />
+            {/* Add routes for login and registration */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </div>
--- moodtrackerfront/src/pages/LoginPage.js
+++ moodtrackerfront/src/pages/LoginPage.js
@@ -1,8 +1,60 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <h1>This is the Login page</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    try {
+      // Send login request to the API
+      const response = await fetch('http://localhost:3000/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+  
+      // Reset form fields and error message
+      setEmail('');
+      setPassword('');
+      setError('');
+  
+      // Redirect or perform any necessary actions upon successful login
+      console.log('Login successful');
+    } catch (error) {
+      setError('Invalid email or password');
+      console.error('Login error:', error);
+    }
+  };
+  
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <button type="submit">Login</button>
+        {error && <p>{error}</p>}
+      </form>
+      <p>Don't have an account? <Link to="/register">Register here</Link></p>
+    </div>
+  );
 }
 
 export default Login;
