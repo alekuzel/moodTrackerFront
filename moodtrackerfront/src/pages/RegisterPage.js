f854418350b122f778982bacdcf25b3e2967d910
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-28 11:14:14 +0100

    register user page

--- moodtrackerfront/src/pages/RegisterPage.js
+++ moodtrackerfront/src/pages/RegisterPage.js
@@ -1,8 +1,69 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import React from 'react';
+function RegistrationPage() {
+  const navigate = useNavigate(); // Hook for navigation
+  const [formData, setFormData] = useState({
+    name: '',
+    lastname: '',
+    email: '',
+    password: ''
+  });
 
-function Register() {
-  return <h1>This is the Register page</h1>;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        // Registration successful, navigate to login page
+        navigate('/login');
+      } else {
+        // Handle registration error
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Lastname:</label>
+          <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default Register;
+export default RegistrationPage;
