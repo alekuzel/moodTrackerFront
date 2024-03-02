fe7e3ddfd497afa5ae00da0416f28d1bd67142b9
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-02 10:06:29 +0100

    design fixes

--- moodtrackerfront/src/components/FooterComponent.js
+++ moodtrackerfront/src/components/FooterComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'; // Import Bootstrap components
-import LogoutButton from './LogoutButton'; // Import the LogoutButton component
-import '../Navigation.css'; // Import custom CSS for styling
+import { Navbar, Nav } from 'react-bootstrap';
+import LogoutButton from './LogoutButton';
+import '../Navigation.css';
 
 function Footer() {
   return (
-    <div className="footer-wrapper">
+    <div className="footer-wrapper" style={{ marginTop: '5vh' }}>
       <Navbar bg="dark" variant="dark" expand="lg" fixed="bottom">
         <Navbar.Brand>Mood tracker. 2024</Navbar.Brand>
         <Nav className="ml-auto">
